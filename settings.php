269221781251e38e2134065efebeea20a71e6bfe
novicaa <novicamiketic@gmail.com>
2015-04-30 13:21:57 +0200

    First commit

---
+++ settings.php
@@ -0,0 +1,11 @@
+<?php
+	$adresa[0]="Julians Deli 200 W. Loveland Ave.Loveland, OH 45140"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad[0]="";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica[0]=""; // AKo ne znate skracenicu ostavite praznu
+	$nazivlokacije[0]="Loveland";
+	$telephone[0]=5135831725;// Broj koji se poziva na dugme CALL US
+
+	$companynumber=1;
+	$theme="b";//Thema koja se aplicira na sve
+	$transitionefect="turn"; //kucate tranziciju koju zelite na stranama . Vrste tranzicija : "fade", "flip", "pop", "slide", "slidedown", "slideup", "turn", "slidefade", "flow"
+?>
